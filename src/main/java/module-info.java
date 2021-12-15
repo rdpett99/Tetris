3db56e2df728107592a6c14219a325ecabcf2a80
Robert Pettigrew (Dylan) <rdp06865@uga.edu>
2021-12-15 16:54:00 -0500

    removing deleted files from tracking

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module cs1302.omega {
     requires transitive java.logging;
     requires transitive javafx.controls;
     requires transitive com.google.gson;
-    exports cs1302.api;
     exports cs1302.game;
     exports cs1302.omega;
 } // module
